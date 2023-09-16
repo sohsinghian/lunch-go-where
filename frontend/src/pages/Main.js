2c5864f0d283d9a7a67b631088e7e61d2b98ea59
sohsinghian <sohsinghian@gmail.com>
2023-09-16 11:19:00 +0800

    install and setup frontend
    installed Tailwindcss, react-router-dom, and created page

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <div className="text-blue-600 text-center font-bold text-6xl">
+        Lunch Go Where
+      </div>
+    </>
+  );
+};
+
+export default Main;
